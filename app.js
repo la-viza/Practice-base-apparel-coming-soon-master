613276b0a7a9c749e0dafdc1f4cda9aa88f4927a
la-viza <97736137+la-viza@users.noreply.github.com>
2022-06-10 16:31:57 +0300

    JS

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const email = document.getElementById("email");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let emailVal = email.value;
+  let emailValue = email.value;
 
-  // check if it is a valid email
-  if (!validateEmail(emailVal)) {
+  if (!validateEmail(emailValue)) {
     form.classList.add("error");
   } else {
     form.classList.remove("error");
-    document.body.innerHTML = `<div style="display:flex; align-items: center; justify-content: center;"><h1>Thank you!</h1></div>`;
+    document.body.innerHTML = `<div style="display:flex; align-items: center; justify-content: center; color:hsl(0, 93%, 68%); margin-top:17rem;
+"><h1>Thank you!</h1></div>`;
   }
 });
 
@@ -18,4 +18,4 @@ function validateEmail(email) {
   var re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
-}
+} 
